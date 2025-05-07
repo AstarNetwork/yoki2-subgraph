393fd197234dc25cb34c0720742f6da46e26948f
Mar.io <34627453+Maar-io@users.noreply.github.com>
2025-05-07 22:03:21 +0200

    works on local

--- scripts/local-check.sh
+++ scripts/local-check.sh
@@ -4,7 +4,7 @@ echo "========== GRAPH NODE STATUS =========="
 curl -s http://localhost:8030/graphql -X POST -d '{"query": "{ indexingStatuses { subgraph health error { message } } }"}' -H "Content-Type: application/json" | jq
 
 echo "\n========== YOKI2 INDEXING PROGRESS =========="
-curl -s http://localhost:8030/graphql -X POST -d '{"query": "{ indexingStatusForCurrentVersion(subgraphName: \"yoki2\") { chains { network { name } latestBlock { number } chainHeadBlock { number } } } }"}' -H "Content-Type: application/json" | jq
+curl -s -X POST -H "Content-Type: application/json" -d '{"query": "{indexingStatusForCurrentVersion(subgraphName: \"yoki2\") {synced health entityCount chains {network latestBlock {number} chainHeadBlock { number } }}}"}' http://localhost:8030/graphql | jq
 
 echo "\n========== REGISTERED SUBGRAPHS =========="
 curl -s -X POST -H "Content-Type: application/json" -d '{"query": "{subgraphs{id name}}"}' http://localhost:8000/graphql | jq
